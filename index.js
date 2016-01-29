0efddf718fdd38842917019bdeb08f63d626226c
jasonkneen <jason@bouncingfish.com>
2016-01-29 20:00:41 +0000

    Support for config vars for appId, restAPIKey, masterKey

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ if (!process.env.DATABASE_URI) {
 var api = new ParseServer({
   databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: 'myAppId',
-  masterKey: 'myMasterKey'
+  appId: process.env.appId || 'myAppId',
+  restAPIKey: process.env.restAPIKey || 'myRestId',
+  masterKey: process.env.masterKey || 'myMasterKey'
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
