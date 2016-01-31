e3c08ffc9813ba11e6b33bf45d529a7e6e364176
Didier Franc <mail@didierfranc.com>
2016-01-31 13:43:26 +0100

    Update index.js
    - Use Express to listen()
    - Add APP_ID & MASTER_KEY to process.env to work OOB with Docker

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-var http = require('http');
 
 if (!process.env.DATABASE_URI) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -12,8 +11,8 @@ if (!process.env.DATABASE_URI) {
 var api = new ParseServer({
   databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: 'myAppId',
-  masterKey: 'myMasterKey'
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || 'myMasterKey'
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -31,7 +30,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 1337;
-var httpServer = http.createServer(app);
-httpServer.listen(port, function() {
-  console.log('parse-server-example running on port ' + port + '.');
+app.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
 });
