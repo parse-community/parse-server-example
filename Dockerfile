b6b459eb1f2a1c736c945afac0e5c1b7c7051461
Melanie Keatley <Melanie@Melanies-MacBook-Air-2.local>
2016-02-03 15:08:22 +0100

    Replaces ENV variables and removes azuredeploy file

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,9 @@ ADD . /parse
 WORKDIR /parse
 RUN npm install
 
-ENV APP_ID setYourAppId
-ENV MASTER_KEY setYourMasterKey
-ENV DATABASE_URI setMongoDBURI
+ENV APP_ID ENV['APP_ID']
+ENV MASTER_KEY ENV['MASTER_KEY']
+ENV DATABASE_URI ENV['DATABASE_URI']
 
 # Optional (default : 'parse/cloud/main.js')
 # ENV CLOUD_CODE_MAIN cloudCodePath
@@ -21,6 +21,6 @@ EXPOSE 1337
 # Uncomment if you want to access cloud code outside of your container
 # A main.js file must be present, if not Parse will not start
 
-# VOLUME /parse/cloud               
+# VOLUME /parse/cloud
 
 CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -11,10 +11,10 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || ENV['DATABASE_URI'],
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'myMasterKey'
+  appId: process.env.APP_ID || ENV['APP_ID'],
+  masterKey: process.env.MASTER_KEY || ENV['MASTER_KEY']
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
