dd47704afadec75bc1b7e7f74bee6303afcebd28
Melanie Keatley <Melanie@Melanies-MacBook-Air-2.local>
2016-02-05 11:36:56 +0100

    Removes Mongolab reference

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URL
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -13,8 +13,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || process.env.MONGODB_URL,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || process.env.APP_ID,
-  masterKey: process.env.MASTER_KEY || process.env.MASTER_KEY
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
