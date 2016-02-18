22b16ee7b8b0510a1c7591da45abfede9cff5365
Alex Chen <chena@cfa.vic.gov.au>
2016-02-18 16:37:34 +1100

    updated index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: 'mongodb://alex:123456@ds039674.mongolab.com:39674/heroku_96k60k3l',
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'wOwmLnQXfeATtJitXVPo96PEisJzzGbAQNI5LUun',
-  restAPIKey: 'Qp10BvrE2j1caGMk196C7XLQ98OTEqeAkb4uSUFf',
-  masterKey: process.env.MASTER_KEY || 'GwJZOgsNquiSXIWkt0r6b79AuFRQQCzKwWxHaEBA' //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'myAppId',
+  restAPIKey: process.env.REST_API_KEY || '',
+  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
