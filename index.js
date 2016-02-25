c86fab41896cfa8a7876e3221a66ea6e8752f7da
Matt Creager <creager@heroku.com>
2016-02-25 09:17:55 -0800

    accept server url env var
    Resolves #74

--- index.js
+++ index.js
@@ -4,13 +4,14 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI
+var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
+  serverURL: process.env.PARSE_SERVER_URL,
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
