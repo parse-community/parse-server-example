d8f7206e1e979b8968605bd8a1c85c715e030c8a
432player <432player@gmail.com>
2016-03-06 16:28:16 +0200

    Added Push Notification + Client?

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,34 @@
+Parse.Cloud.define('pushChannelTest', function(request, response) {
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+  // request has 2 parameters: params passed by the client and the authorized user
+  var params = request.params;
+  var user = request.user;
+
+  // extract out the channel to send
+  var action = params.action;
+  var message = params.message;
+  var customData = params.customData;
+
+  // use to custom tweak whatever payload you wish to send
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo("deviceType", "android");
+  pushQuery.equalTo("deviceType", "ios");
+
+  var payload = {"data": {
+      "alert": message,
+      "action": action,
+      "customdata": customData}
+                };
+
+  // Note that useMasterKey is necessary for Push notifications to succeed.
+
+  Parse.Push.send({
+  where: pushQuery,      // for sending to a specific channel                                                                                                                                 data: payload,
+  }, { success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
 });
