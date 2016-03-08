70df51cf0711d5e1d474c2d19596327dd4aa60be
432player <432player@gmail.com>
2016-03-08 18:15:19 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -21,44 +21,44 @@ Parse.Cloud.define('pushChannelMedidate', function(request, response) {
   console.log("#### Push Type " + push_type);
 
   //Filter only users with thier ids in it
-  // var userQuery = new Parse.Query(Parse.User);
-  // userQuery.containedIn("objectId", users);
-  // for (var i = 0; i < users.length; i++) {
-  //   console.log("#### User Id " + users[i]);
-  // }
+  var userQuery = new Parse.Query(Parse.User);
+  userQuery.containedIn("objectId", users);
+  for (var i = 0; i < users.length; i++) {
+    console.log("#### User Id " + users[i]);
+  }
 
   var pushQuery = new Parse.Query(Parse.Installation);
-  // switch (push_type) {
-  //     case 0:
-  //         pushQuery.equalTo("session_changed_push", true);
-  //         console.log("#### session_changed_push");
-  //         break;
-  //     case 1:
-  //         pushQuery.equalTo("user_followed_push", true);
-  //         console.log("#### user_followed_push");
-  //         break;
-  //     case 2:
-  //         pushQuery.equalTo("session_attender_push", true);
-  //         console.log("#### session_attender_push");
-  //         break;
-  //     case 3:
-  //         pushQuery.equalTo("new_follower_push", true);
-  //         console.log("#### new_follower_push");
-  //         break;
-  //     case 4:
-  //         //NOTHING TO FILTER
-  //         console.log("#### session_deleted_push");
-  //         break;
-  //     case 5:
-  //         pushQuery.equalTo("session_message_push", true);
-  //         console.log("#### session_message_push");
-  //         break;
-  //     default:
-  //         pushQuery.equalTo("session_changed_push", true);
-  //         console.log("#### session_changed_push");
-  //         break;
-  // }
-  // pushQuery.containedIn("user", userQuery);
+  switch (push_type) {
+      case 0:
+          pushQuery.equalTo("session_changed_push", true);
+          console.log("#### session_changed_push");
+          break;
+      case 1:
+          pushQuery.equalTo("user_followed_push", true);
+          console.log("#### user_followed_push");
+          break;
+      case 2:
+          pushQuery.equalTo("session_attender_push", true);
+          console.log("#### session_attender_push");
+          break;
+      case 3:
+          pushQuery.equalTo("new_follower_push", true);
+          console.log("#### new_follower_push");
+          break;
+      case 4:
+          //NOTHING TO FILTER
+          console.log("#### session_deleted_push");
+          break;
+      case 5:
+          pushQuery.equalTo("session_message_push", true);
+          console.log("#### session_message_push");
+          break;
+      default:
+          pushQuery.equalTo("session_changed_push", true);
+          console.log("#### session_changed_push");
+          break;
+  }
+  pushQuery.containedIn("user", userQuery);
 
   // Note that useMasterKey is necessary for Push notifications to succeed.
   Parse.Push.send({
