4bfbbbfe2d661e63ad2547f636190fcc69fed348
432player <432player@gmail.com>
2016-03-09 19:10:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,13 +57,13 @@ var api = new ParseServer({
 	// 	production: true
 	// 	}
 	// }
-  // ,
-  // customPages: {
-  //   invalidLink: process.env.SERVER_URL + 'invalid_link.html',
-  //   verifyEmailSuccess: process.env.SERVER_URL + 'verify_email_success.html',
-  //   choosePassword: process.env.SERVER_URL + 'views/choose_password',
-  //   passwordResetSuccess: process.env.SERVER_URL + 'password_reset_success.html'
-  // }
+   ,
+   customPages: {
+     invalidLink: process.env.SERVER_URL + 'invalid_link.html',
+     verifyEmailSuccess: process.env.SERVER_URL + 'verify_email_success.html',
+     choosePassword: 'http://matandahan.wix.com/medidate#!reset-password/ldxuc',
+     passwordResetSuccess: process.env.SERVER_URL + 'password_reset_success.html'
+   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
