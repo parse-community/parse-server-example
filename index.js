4ee01168c08a12ac369f0b57064f229b2e4307e2
432player <432player@gmail.com>
2016-03-10 13:10:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var simpleMailgunAdapter = require('parse-server/lib/Adapters/Email/SimpleMailgu
 // });
 
 //Push Adapter
-var OneSignalPushAdapter = require('parse-server/lib/Adapters/Push/OneSignalPushAdapter');
+var OneSignalPushAdapter = require(__dirname + '/adapters/OneSignalPushAdapter');
 var oneSignalPushAdapter = new OneSignalPushAdapter({
   oneSignalAppId:process.env.ONE_SIGNAL_APP_ID,
   oneSignalApiKey:process.env.ONE_SIGNAL_REST_API_KEY
