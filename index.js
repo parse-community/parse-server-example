b1d544a709764cba3a095b9f6f2460014f46da87
Melanie Keatley <Melanie@Melanies-MacBook-Air-2.local>
2016-03-10 15:54:00 +0100

    Add Amazon variables

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var S3Adapter = require('parse-server').S3Adapter;
+
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URL
 
@@ -15,6 +17,13 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID,
   masterKey: process.env.MASTER_KEY
+
+  filesAdapter: new S3Adapter(
+    process.env.AWS_ACCESS_KEY_ID,
+    process.env.AWS_SECRET_ACCESS_KEY,
+    process.env.BUCKET_NAME,
+    {directAccess: true}
+  ),
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
