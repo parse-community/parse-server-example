c3dcba2ceb310a7f15e8880f07d4329c2af8c53a
Edgard <edmbayen@gmail.com>
2016-03-14 17:40:58 +0100

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var api = new ParseServer({
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
-
+ 
 var app = express();
 
 // Serve the Parse API on the /parse URL prefix
