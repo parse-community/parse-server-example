744cab4d8e96d9a8841876ed012f5cf9721e6d98
Boris Adimov <boris@adimov.ru>
2016-03-14 15:30:37 +0300

    fixed an error onStart

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ var S3Adapter = require('parse-server').S3Adapter;
 var corser = require("corser");
 
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URL || 'mongodb://localhost:27017/dev'
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URL
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || process.env.MONGODB_URL || 'mongodb://localhost:27017/dev' ,
+  databaseURI: databaseUri || process.env.MONGODB_URL || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || '1234',
   masterKey: process.env.MASTER_KEY || '1234',
-  serverURL: process.env.SERVER_URL || 'http://localhost:49152',
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337',
 
   filesAdapter: new S3Adapter(
     process.env.AWS_ACCESS_KEY_ID,
@@ -44,6 +44,8 @@ app.all('*', function(request, response, next) {
     response.header('Access-Control-Allow-Origin', '*');
     next();
 });
+
+
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
@@ -53,7 +55,7 @@ app.get('/', function(req, res) {
   res.status(200).send('I dream of being a web site.');
 });
 
-var port = process.env.PORT || 49152;
+var port = process.env.PORT || 1337;
 app.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
