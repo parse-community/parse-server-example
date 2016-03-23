62144d5fa0a2aee7df375d09906919c98957eaf4
Dan Dietz <paperreduction@users.noreply.github.com>
2016-03-23 16:22:57 -0400

    Debug: removing ParseServer.createLiveQueryServer
    Trying to debug why the parse server won't start on heroku. Removing call to createLiveQueryServer from index.js since it is giving a error.

--- index.js
+++ index.js
@@ -52,5 +52,5 @@ httpServer.listen(port, function() {
 });
 
 // This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
+//ParseServer.createLiveQueryServer(httpServer);
 
