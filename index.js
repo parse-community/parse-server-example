cfdb78c6823c8d04a62379334afd1af1f1be2e09
Chris Li <chris.li.2013@hotmail.com>
2016-03-29 13:35:23 -0700

    Revert "Try to fix node.js start problem"
    This reverts commit e1f8fb0fe61171bf181235975e75f2659beab1db.

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.PARSE_APP_ID || 'myAppId',
-  masterKey: process.env.PARSE_MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: 'http://localhost:1337/parse' // Don't forget to change to https if needed
+  masterKey: process.env.PARSE_MASTER_KEY || '' //Add your master key here. Keep it secret!
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
