ecf7fb9f37b8b7ed8d7b4b5a718eba52d821b4dd
renishghetia <renishghetia@gmail.com>
2016-04-20 17:33:42 +0530

    test

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,4 @@
-var PubNub = require('pubnub')({
+var pubnub = require('pubnub')({
 ssl:true,
 publish_key : 'pub-c-cfa5a241-8cb8-4263-a498-394e2d385909',
 subscribe_key : 'sub-c-e3913a52-0462-11e6-8c3e-0619f8945a4f'
