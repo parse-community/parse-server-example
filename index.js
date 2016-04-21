d2b5dff38d4fa9973ea9958e25d1a95ffdcffd3f
Willian Ribeiro Angelo <agfoccus@gmail.com>
2016-04-21 14:14:09 -0300

    Heroku Deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI || process.env.MONGOLAB_URI;
+var databaseUri = process.env.MONGODB_URI || process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
