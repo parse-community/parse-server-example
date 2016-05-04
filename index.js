5c1a7bca62ed1884a7d63ebc02e418a1748f4956
Fosco Marotto <gfosco@gmail.com>
2016-05-04 13:09:42 -0700

    Added cheeky line back to default route.
    Was used in a raywenderlich.com tutorial and Héctor mentioned that it may confuse people if it's not there.

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('Make sure to star the parse-server repo on GitHub!');
+  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
 // There will be a test page available on the /test path of your server url
