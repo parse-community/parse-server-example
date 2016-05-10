9c03756f4373b4a00ba1e057e7de9334bfa9a8e6
Matt Mason <mamaso@microsoft.com>
2016-05-10 15:51:27 -0700

    Fixing errors from package cleaning

--- package.ps1
+++ package.ps1
@@ -5,7 +5,7 @@ npm i --production
 $path = "node_modules"
 Get-ChildItem -Path $path -Recurse | where {$_.PSIsContainer} | Where-Object {$_.Name -match '^(tests?|samples|docs?|obj|typings|examples?|jsdoc)$'} | Remove-Item -Recurse -Verbose
 Get-ChildItem -Path $path -Recurse | where {!$_.PSIsContainer} | Where-Object {$_.Name -match '\.(coffee|md|png|jpg|pdb|travis\.yml|gitignore|npmignore|d\.ts)$'} | Remove-Item -Recurse -Verbose
-Get-ChildItem -Path $path -Recurse | where {!$_.PSIsContainer} | Where-Object {$_.Name -match '\.*(test|example).*$'} | Remove-Item -Recurse -Verbose
+Get-ChildItem -Path $path -Recurse | where {!$_.PSIsContainer} | Where-Object {$_.Name -match '\.*(example).*$'} | Remove-Item -Recurse -Verbose
 
 function ZipFiles( $zipfilename, $sourcedir )
 {
