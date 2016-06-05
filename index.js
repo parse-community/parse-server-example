fc493958e366a06232f8823022ff32334a963b93
muk242 <muk242@hotmail.com>
2016-06-05 16:29:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var S3Adapter = require('parse-server').S3Adapter;
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
