59b157f3c1ab63335da7caf8d66e90fe2a4b7cfd
samllchiang <samllchiang@hotmail.com>
2016-06-21 13:34:24 +0800

    --- xxx2

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.use(mountPath, api);
 app.get('/', function(req, res) {
   res.status(200).send('I dream of being a web site.');
 });
-//-----------------------------------xxx1
+//-----------------------------------xxx2
 var port = process.env.PORT || 1337;
 app.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
