52fc6b248fec3a76a5338904738ef165b0b59f19
rycardo <rycardo@users.noreply.github.com>
2016-08-15 17:11:36 -0400

    Added additional files for bsd and twilio files

---
+++ cloud/app.js
@@ -0,0 +1,121 @@
+
+// These two lines are required to initialize Express in Cloud Code.
+ express = require('express');
+ app = express();
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// This is an example of hooking up a request handler with a specific request
+// path and HTTP verb using the Express routing API.
+// app.get('/hello', function(req, res) {
+//   res.render('hello', { message: 'Congrats, you just set up your app!' });
+// });
+
+// // Example reading from the request query string of an HTTP get request.
+app.get('/test', function(req, res) {
+  // GET http://example.parseapp.com/test?message=hello
+  res.send("test is displayed");
+  //res.send(req.query.message);
+});
+
+// // Example reading from the request body of an HTTP post request.
+
+
+app.post('/twilio', function(req, res)
+{
+   // POST http://example.parseapp.com/test (with request body "message=hello")
+   var messageBody = req.params.Body;
+   var messageFrom = req.params.From;
+
+   Parse.Cloud.useMasterKey();
+
+	var userAgent       = 'Indigo SMS Service 0.0.0 (beta)'
+
+    var messageBody     = request.params.Body;
+    var messageFrom     = request.params.From;
+
+    var dateObject      = new Date();
+    var timeStamp       = dateObject.getUTCDate();
+
+	var rycardo = {
+					baseURL: "http://tdot.coubur.com",
+					port: '8176',
+					username: 'indigo',
+					password: '1nd190Server',
+					from: 'twilioFrom',
+					time: 'twilioTimeStamp',
+					message: 'twilioMessage'
+					};
+
+	var account = {
+					name: '16172165525',
+					info: rycardo
+					}
+
+	var accounts = [account];
+
+	var messageAccount = null;
+
+	for ( a = 0; a < accounts.count; a++ )
+	{
+		var thisAccount = accounts[a];
+		if ( thisAccount["name"] == messageFrom )
+		{
+			messageAccount = thisAccount["info"];
+			break;
+		}
+	}
+
+	if ( messageAccount == null )
+	{
+		response.error("not authorised");
+	}
+
+	var baseURL		= messageAccount["baseURL"];
+	var port		= messageAccount["port"];
+	var username	= messageAccount["username"];
+	var password	= messageAccount["password"];
+	var fromVar		= messageAccount["from"];
+	var timeVar		= messageAccount["time"];
+	var messageVar	= messageAccount["message"];
+
+
+	var extURL 	= username + ":" + password + "@" + baseURL + ":" + port + "/variables/";
+
+	var fromURL = extURL + fromVar;
+	var timestampURL = extURL + timeVar;
+	var bodyURL = extURL + messageVar;
+
+	Parse.Cloud.httpRequest(
+	{
+		method: 'POST',
+		url: fromURL,
+		followRedirects: true,
+		headers:
+		{
+			'UserAgent': userAgent
+		},
+		params:
+		{
+			_method : 'put',
+			value : messageFrom
+		}
+	}).then(function(httpResponse)
+	{
+		// Success
+		response.success(httpResponse.text);
+
+	}, function(httpResponse)
+	{
+		// error
+		response.error(httpResponse.status);
+	});
+
+   //res.send(req.body.message);
+ });
+
+// Attach the Express app to Cloud Code.
+app.listen();
---
+++ cloud/twil.js
@@ -0,0 +1,170 @@
+var express = require("express");
+var app = express();
+
+var language = "en";
+var languages = ["en"];//, "es", "ja", "kr", "pt-BR"];
+
+var strings = require("cloud/strings.js");
+
+app.set('views', 'cloud/views');
+app.set('view engine', 'ejs');
+app.use(express.bodyParser());
+
+function extend(target) {
+    var sources = [].slice.call(arguments, 1);
+    sources.forEach(function (source) {
+        for (var prop in source) {
+            target[prop] = source[prop];
+        }
+    });
+    return target;
+}
+
+app.get('/', function(req, res) {
+  var lang = req.query.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+    language = lang;
+  }
+
+  res.render("index.ejs", extend(strings[language], {'language': language}));
+});
+
+
+app.post('/', function(req, res) {
+
+  var form = req.body;
+
+  var phoneNumber = form.phoneNumber;
+
+  var lang = form.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+    language = lang;
+  }
+
+  if(phoneNumber) {
+    phoneNumber = phoneNumber.replace(/\D/g, '');
+    // check that length of number is 10 for US, 11 for Japan or Korea, 10 or 11 for Brazil
+    if(language == "en" && phoneNumber.length != 10
+      || ((language == "ja" || language == "kr") && phoneNumber.length != 11)
+      || (language == "pt-BR" && phoneNumber.length != 10 && phoneNumber.length != 11)) {
+      res.render("index.ejs", extend(strings[language], {warning: strings[language]['warningPhone'], 'language': language}));
+    }
+    Parse.Cloud.run("sendCode", {phoneNumber: phoneNumber, language: language}).then(function(response){
+      if(!response){
+        res.render("verify.ejs", extend(strings[language], {phoneNumber: phoneNumber, 'language': language}));
+      } else {
+        res.render("index.ejs", extend(strings[language], {warning: strings[language]['warningTwilio'], 'language': language}));
+      }
+    }, function(error){
+      res.render("index.ejs", extend(strings[language], {warning: strings[language]['warningGeneral'], 'language': language}));
+    });
+  } else {
+    res.render("index.ejs", extend(strings[language], {warning: strings[language]['warningNoNumber'], 'language': language}))
+  }
+});
+
+app.get('/verify', function(req, res){
+  res.redirect("/");
+})
+
+app.post('/verify', function(req, res){
+  var form = req.body;
+  var code = form.code;
+  var phoneNumber = form.phoneNumber;
+
+  var lang = form.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+    language = lang;
+  }
+
+  if(code) {
+    code = code.replace(/\D/g, '');
+    if(code.length != 4) {
+      res.render("verify.ejs", extend(strings[language], {warning: strings[language]['warningCodeLength'], 'language': language}));
+    }
+    Parse.Cloud.run("logIn", {codeEntry: code, phoneNumber: phoneNumber}).then(function(response){
+      var sessionToken = response;
+      Parse.User.become(sessionToken).then(function (user) {
+        res.redirect('/dashboard/' + sessionToken)
+      }, function (error) {
+        res.redirect('/');
+      });
+
+    }, function(error) {
+      res.render("verify.ejs", extend(strings[language], {phoneNumber: phoneNumber, warning: strings[language]['warningGeneral'], 'language': language}));
+    });
+  } else {
+    res.render("verify.ejs", extend(strings[language], {phoneNumber: phoneNumber, warning:strings[language]['warningCodeInvalid'], 'language': language}));
+  }
+});
+
+app.get('/dashboard/:sessionToken', function(req, res) {
+  var sessionToken = req.params.sessionToken;
+
+  Parse.User.become(sessionToken).then(function (user) {
+    console.log("logged in as user");
+
+    language = user.get("language");
+
+    res.render("dashboard.ejs", extend(strings[language], {
+      sessionToken: sessionToken,
+      phoneNumber: user.get("username"),
+      name: user.get("name"),
+      setting1: user.get("setting1"),
+      setting2: user.get("setting2"),
+      setting3: user.get("setting3"),
+      language: user.get("language"),
+    }));
+  }, function (error) {
+    console.log("Could not log in as user.");
+    res.redirect("/");
+  });
+});
+
+app.post('/dashboard', function(req, res) {
+  var form = req.body;
+
+  var sessionToken = form.sessionToken;
+
+  var name = form.name;
+
+  var setting1 = form.setting1;
+  var setting2 = form.setting2;
+  var setting3 = form.setting3;
+
+  var lang = form.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+    language = lang;
+  }
+
+  Parse.User.become(sessionToken).then(function (user) {
+    user.set("name", name);
+    user.set("setting1", (setting1 !== undefined && setting1 == "on")?true:false);
+    user.set("setting2", (setting2 !== undefined && setting2 == "on")?true:false);
+    user.set("setting3", (setting3 !== undefined && setting3 == "on")?true:false);
+    user.set("language", language);
+    user.save().then(function(user) {
+
+      language = user.get("language");
+
+      res.render("dashboard.ejs", extend(strings[language], {
+        sessionToken: sessionToken,
+        phoneNumber: user.get("username"),
+        name: user.get("name"),
+        setting1: user.get("setting1"),
+        setting2: user.get("setting2"),
+        setting3: user.get("setting3"),
+        language: user.get("language"),
+      }));
+    }, function(error) {
+      console.log(error);
+      res.redirect("/");
+     });
+  }, function (error) {
+    console.log(error);
+    res.redirect("/");
+  });
+
+});
+
+app.listen();
