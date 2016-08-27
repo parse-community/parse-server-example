4264c2539bbaa2bc1a8bffe59cfb1da8a5b2cb03
Bin Lin <cold.lin@gmail.com>
2016-08-27 06:47:20 +0100

    disable s3 file adapter disable live query

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-var S3Adapter = require('parse-server').S3Adapter;
+//var S3Adapter = require('parse-server').S3Adapter;
 
 var path = require('path');
 
@@ -19,16 +19,16 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   fileKey: process.env.FILE_KEY || 'myFileKey',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-  filesAdapter: new S3Adapter(
-      process.env.AWS_ACCESS_KEY_ID ||"AWS_ACCESS_KEY_ID",
-      process.env.AWS_SECRET_ACCESS_KEY ||"AWS_SECRET_ACCESS_KEY",
-      process.env.AWS_S3_BUCKET_NAME || 'BUCKET_NAME',
-      { directAccess: true }
-    ),
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse'  // Don't forget to change to https if needed
+//  liveQuery: {
+//    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+//  },
+//  filesAdapter: new S3Adapter(
+//      process.env.AWS_ACCESS_KEY_ID ||"AWS_ACCESS_KEY_ID",
+//      process.env.AWS_SECRET_ACCESS_KEY ||"AWS_SECRET_ACCESS_KEY",
+//      process.env.AWS_S3_BUCKET_NAME || 'BUCKET_NAME',
+//      { directAccess: true }
+//    ),
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -54,11 +54,11 @@ app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
-var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
-});
+//var port = process.env.PORT || 1337;
+//var httpServer = require('http').createServer(app);
+//httpServer.listen(port, function() {
+//    console.log('parse-server-example running on port ' + port + '.');
+//});
 
 // This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
+//ParseServer.createLiveQueryServer(httpServer);
