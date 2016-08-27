bab3ddf6cba05f9cc2483374f710b8e7832c43ee
Bin Lin <cold.lin@gmail.com>
2016-08-27 06:51:10 +0100

    disable s3 file adapter disable live query

--- index.js
+++ index.js
@@ -54,11 +54,11 @@ app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
-//var port = process.env.PORT || 1337;
-//var httpServer = require('http').createServer(app);
-//httpServer.listen(port, function() {
-//    console.log('parse-server-example running on port ' + port + '.');
-//});
+var port = process.env.PORT || 1337;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
 
 // This will enable the Live Query real-time server
 //ParseServer.createLiveQueryServer(httpServer);
