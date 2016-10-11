baad01b609b9eb5903d731da4df2fbae8b3edccf
Kyle Parent <kjpparent@gmail.com>
2016-10-10 22:43:08 -0700

    Add app id and master key to index

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'HamrD3OpwqZ7rpqNYCVCvWiFkhy8LpQGrrdcxTvE',
+  masterKey: process.env.MASTER_KEY || 'bVH8zxxRM7EMNALMKqIdBTf5XUd7S6txfWy6ogPE', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
