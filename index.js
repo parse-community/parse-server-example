b84c9e43cc1bdfd0a1cb90f07a9d72c8de4be66b
CodeNpk <codenpk@gmail.com>
2016-10-20 07:53:48 +0000

    optional client key

--- index.js
+++ index.js
@@ -16,8 +16,11 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
+  clientKey: process.env.CLIENT_KEY || '', //optional
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  fileKey : process.env.FILE_KEY || '',
+  accountLockout : true,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
@@ -37,6 +40,7 @@ const dashboard = new ParseDashboard({
       "appName": process.env.APP_NAME,
       "user": "username",
       "pass": process.env.PARSE_DASHBOARD_ADMIN_PASSWORD || "password"
+      "production": true
     }
   ],
   "users": [
