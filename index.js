536d6db46173b4b04de4b57b461ff052095ab51a
CodeNpk <codenpk@gmail.com>
2016-10-20 05:43:28 +0000

    fix for node

--- index.js
+++ index.js
@@ -35,11 +35,18 @@ const dashboard = new ParseDashboard({
       "serverURL": process.env.SERVER_URL || 'http://localhost:1337/parse',
       "appId": process.env.APP_ID || 'myAppId',
       "masterKey": process.env.MASTER_KEY || '',
-      "appName": "appName",
+      "appName": process.env.APP_NAME,
       "user": "username",
       "pass": "password"
     }
+  ],
+  "users": [
+    {
+      "user":"username",
+      "pass":"password" // change this to something secure
+    }
   ]
+
 }, true);
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -53,13 +60,13 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
-var dashMountPath = process.env.PARSE_DASHBOARD_MOUNT || '/dashboard';
+var dashMountPath = process.env.PARSE_DASHBOARD_MOUNT || '/';
 app.use(dashMountPath, dashboard);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.');
-});
+});*/
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
