7185b0ae5ac7b9d7b9ff37eca0b59e6d3a87bf72
CodeNpk <codenpk@gmail.com>
2016-10-20 05:09:17 +0000

    deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-var ParseDashboard = require('parse-dashboard')
+var ParseDashboard = require('parse-dashboard');
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
