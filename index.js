d874a229ea73a82119ef8aa4b53a9b6d487914a2
CodeNpk <codenpk@gmail.com>
2016-10-20 05:35:45 +0000

    fix to unauthorised 1

--- index.js
+++ index.js
@@ -39,13 +39,7 @@ const dashboard = new ParseDashboard({
       "user": "username",
       "pass": "password"
     }
-  ],
-  "users": [
-     {
-       "user": "username",
-       "pass": "password"
-     }
-   ]
+  ]
 }, true);
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
