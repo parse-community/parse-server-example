d4275ae267e15bf6d16af969fd062596fae3d743
CodeNpk <codenpk@gmail.com>
2016-10-20 04:17:56 +0000

    Changed

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.status(200).send('I dream of being a website.');
 });
 
 // There will be a test page available on the /test path of your server url
