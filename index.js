373320d4fdb4be72dc9fb08b7e853876e692fc15
Kyle Parent <kjpparent@gmail.com>
2016-10-22 17:57:32 -0700

    Don't reject unauthorized

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
