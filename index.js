e3c68e43d3e0b4b3042d9065e45cbbf99d681337
chandrahh <chandra.hh@gmail.com>
2016-10-30 16:02:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,18 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  push: {
-        android: {
-          senderId: process.env.GCM_SENDER_ID || '',
-          apiKey: process.env.GCM_API_KEY || ''
-        },
+    push: {
+      android: {
+        senderId: '...',
+        apiKey: '...'
       },
+      ios: {
+        pfx: '/file/path/to/XXX.p12',
+        passphrase: '', // optional password to your p12/PFX
+        bundleId: '',
+        production: false
+      }
+    },
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
