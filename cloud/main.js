bb83d1ca4a2ebf9c5414c8a6e3ea0ca8b856c9db
rycardo <rycardo@users.noreply.github.com>
2016-11-06 16:51:04 -0500

    Removed Twilio funcs

--- cloud/main.js
+++ cloud/main.js
@@ -442,21 +442,21 @@ Parse.Cloud.define('getUnreadMessageCount', function(request, response)
 	// Unread Messages
 	var query = new Parse.Query('Messages');
 	var recipientID = request.params.installId;
-	conditionalLog('Recipient: [' . recipientID . ']');
-	
+
+	console.log('Getting Unread Messages Count for recipient [' . recipientID . ']');
+
 	query.equalTo('recipientID', recipientID);
 	query.doesNotExist('readAt');
-	conditionalLog('About to get');
 	query.get(
 	{
-		success: function(result)
+		success: function(results)
 		{
-			conditionalLog('SUCCESS: ' . result.count);
-			response.success(result.count);
+			console.log('SUCCESS: ' . results.count);
+			response.success(results.count);
 		},
-		error: function(err)
+		error: function(error)
 		{
-			conditionalLog('ERROR: ' . err);
+			console.log('ERROR: ' . error);
 			response.error('unable to get unread messages');
 		}
 	});
@@ -521,179 +521,8 @@ Parse.Cloud.define('convertMessagesFromDeviceToUser', function(request, response
 function conditionalLog(logText)
 {
 	var doLog = env.process.DEBUG_LOG
-	if ( doLog == "True" )
+	if ( doLog == True || doLog == "True" )
 	{
 		console.log(logText);
 	}
-}
-
-///////////////////////////////////////
-//
-// randomNumberWithNumberOfDigits - not public
-//
-///////////////////////////////////////
-function randomNumberWithNumberOfDigits(numDigits)
-{
-	var num = '';
-
-	for(d = 0; d < numDigits; d++)
-	{
-		var min = 0;
-		var max = 9;
-		var digit = Math.floor(Math.random() * (max - min + 1)) + min;
-
-		num = num + digit.toString();
-	}
-
-	return num;
-}
-
-///////////////////////////////////////
-//
-// Twilio Account Info
-//
-///////////////////////////////////////
-var twilioAccountSid    = 'AC31fd04d3ce8f6369308dc42d2cb16559';
-var twilioAuthToken     = 'f20e0042726b0d9cca9e017e68d1f579';
-var twilioPhoneNumber   = '+16172199117';
-var secretPasswordToken = '4B9CAEC3-E9BF-42D0-B57B-6109D90A4E07';
-var twilioMessagingSID  = 'MGb685587f2b87ff3e94536ef258cfced9';
-var language            = "en";
-var languages           = ["en","en-GB"];
-
-var twilio = require('twilio')(twilioAccountSid, twilioAuthToken);
-
-var debugging 		= true;
-
-///////////////////////////////////////
-//
-// getTwilioPhoneNumber
-//
-///////////////////////////////////////
-Parse.Cloud.define('getTwilioPhoneNumber', function(request, response)
-{
-	Parse.Cloud.useMasterKey();
-
-	response.success(twilioPhoneNumber);
-});
-
-///////////////////////////////////////
-//
-// sendCode
-//
-///////////////////////////////////////
-Parse.Cloud.define('sendCode', function(req, res) 
-{
-	conditionalLog(debugging, 'sendCode function called');
-	
-	var phoneNumber = req.params.phoneNumber;
-	phoneNumber 	= phoneNumber.replace(/\D/g, '');
-	conditionalLog(debugging, 'phoneNumber: [' + phoneNumber + ']');
-
-	var resend	= req.params.resend;
-	conditionalLog(debugging, 'resend: [' + resend + ']');
-
-	var lang 	= req.params.language;
-	conditionalLog(debugging, 'lang: [' + language + ']');
-
-	if ( lang !== undefined && languages.indexOf(lang) != -1 )
-	{
-		language = lang;
-		conditionalLog(debugging,'language set to default');
-	}
-
-	if ( !phoneNumber || phoneNumber.length != 10 )
-	{
-		conditionalLog(debugging,'invalid phone number [' + phoneNumber + ']');
-		return res.error('Invalid Phone number');
-	}
-
-	Parse.Cloud.useMasterKey();
-
-	var query = new Parse.Query(Parse.User);
-
-	query.equalTo('username', phoneNumber + '');
-	query.first().then(function(result)
-	{
-		var num = randomNumberWithNumberOfDigits(5);
-		conditionalLog(debugging, 'will send 5 digit verification number');
-		if ( result )
-		{
-			var expiry = result.get('alternateDeviceExpiry');
-			var isLinking = isWithinFiveMinutes(expiry);
-
-			if ( resend != null)
-			{
-				conditionalLog(debugging,'resend request');
-				isLinking = false;
-			}
-
-			if ( isLinking )
-			{
-				conditionalLog(debugging,'linking, will obtain from other device');
-				res.success('obtain verification code from existing device');
-			}
-			else
-			{
-				result.setPassword(secretPasswordToken + '-' + num);
-				result.set('language', language);
-				result.save().then(function()
-				{
-					conditionalLog(debugging,'ready to send code to phone');
-					return sendCodeSms(phoneNumber, num, language);
-				}).then(function()
-				{
-					conditionalLog(debugging, 'sent code');
-					res.success("verification code sent");
-				}, 
-				function(err)
-				{
-					res.error(err);
-				})
-			};
-		} else
-		{
-			return res.error('No user found with phone number');
-		}
-	}, function (err)
-	{
-		res.error(err);
-	});
-});
-
-///////////////////////////////////////
-//
-// sendCodeSms
-//
-///////////////////////////////////////
-function sendCodeSms(phoneNumber, code, language) 
-{
-	var prefix = '+1';
-	if ( typeof language !== undefined && language == 'en-GB')
-    	{
-		// UK Add +44 and remove 0
-		// 07547 835 213 becomes +44 7547 835 213
-		prefix = "+44";
-		phoneNumber = phoneNumber.substring(1);
-	}
-
-	var promise = new Parse.Promise();
-	twilio.sendSms(
-	{
-		to: prefix + phoneNumber.replace(/\D/g, ''),
-		from: twilioPhoneNumber.replace(/\D/g, ''),
-		body: 'Your Verification Code for the Barbershop Deluxe app is ' + code
-	}
-	,function(err, responseData)
-	{
-		if (err) {
-			console.log(err);
-			promise.reject(err.message);
-		}
-		else
-		{
-			promise.resolve();
-		}
-	});
-	return promise;
 }
