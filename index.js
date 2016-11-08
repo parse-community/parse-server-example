3ddcadb0f30dd755c13abed89aeb10ac27f86957
rycardo <rycardo@users.noreply.github.com>
2016-11-08 07:40:26 -0500

    removed liveQuery lines

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["GlobalSettings"] // List of classes to support for query subscriptions
-  }
+  //liveQuery: {
+  //  classNames: ["GlobalSettings"] // List of classes to support for query subscriptions
+  //}
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -56,4 +56,4 @@ httpServer.listen(port, function() {
 });
 
 // This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
+//ParseServer.createLiveQueryServer(httpServer);
