dc512357cd2469c46f54910f106cbe8aaa85755f
Lin, Bin [R3PI] <Bin.Lin@r3pi.io>
2016-11-08 09:38:57 +0000

    return user rank and score

--- cloud/main.js
+++ cloud/main.js
@@ -290,6 +290,7 @@ Parse.Cloud.define("updateUserStats", function(request, response) {
     userQuery.find({
     		useMasterKey:true,
     		success: function(results) {
+				console.log("user = " + results[0]);
     			var user = results[0];
     			var totalAppUseTimeScore = user.get("timePlayedTotal")/10 || 0;
                 if(totalAppUseTimeScore > 500) {
