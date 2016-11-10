199dd82f168e4372e3163f30258bacd4d074cd20
Rycardo <rycardo.cousteau@yahoo.ca>
2016-11-09 19:34:19 -0500

    added twilio start code.
    added the code to start up twilio with basics.

--- index.js
+++ index.js
@@ -50,27 +50,63 @@ var app = express();
 // twilio.initialize(twilioAccountSid,twilioAccountToken);
 
 
+// Static Assets
+//
+// Twilio mount /twilio
+// Serve static assets from the /cloud/twilio folder
+app.use('/twilio', express.static(path.join(__dirname, '/cloud/twilio')));
+
+
+// Public mount /public
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
+// Parse mount /barbershop
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
+// GETS
+//
+// no path - Parse default
+//
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
   res.status(200).send('I am not really dreaming of being a website, instead I am dreaming of being an app back end!');
 });
 
+// Test returns test.html
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
-var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
+
+// Create a route that will respond to am HTTP GET request with some
+// simple TwiML instructions
+app.get('/hello', function(request, response) {
+    // Create a TwiML response generator object
+    var twiml = new twilio.TwimlResponse();
+
+    // add some instructions
+    var sayWhat = 'Hello, you have called the messaging number for App Support, we do not accept voice calls. If you need assistance, please use Help and Support from the menu of the app you are using. If you are not able to open the app, you can send an email to App Support at Barbershop Deluxe dot com. Thank you. This call is now disconnecting.';
+
+    twiml.say(sayWhat,
+    {
+        voice:'man'
+    });
+
+    // Render the TwiML XML document
+    response.type('text/xml');
+    response.send(twiml.toString());
+});
+
+var port 		= process.env.PORT || 1337;
+var httpServer 	= require('http').createServer(app);
+
+httpServer.listen(port, function()
+{
     console.log('parse-server running on port ' + port + '.');
 });
 
