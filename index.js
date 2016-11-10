9ce223f92e3f949f83077d35e9782954fa291aa0
Rycardo <rycardo.cousteau@yahoo.ca>
2016-11-09 19:22:41 -0500

    added some initial twilio code.

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+// Express Always Required
+var express 	= require('express');
 
+// Parse Service
+var ParseServer = require('parse-server').ParseServer;
+var path 		= require('path');
+
+// Twilio Service
+var twilio		= require('twilio');
+
+//
+// Parse Init
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
-if (!databaseUri) {
+if (!databaseUri)
+{
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var api = new ParseServer({
+var api = new ParseServer(
+{
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
@@ -21,14 +31,23 @@ var api = new ParseServer({
   //  classNames: ["GlobalSettings"] // List of classes to support for query subscriptions
   //}
 });
+
+//
+// Twilio Init
+var twilioAccountSid 	= process.env.TWILIO_ACCOUNT_SID;
+var twilioAccountToken  = process.env.TWILIO_ACCOUNT_TOKEN;
+
+var twilio = require('twilio')(twilioAccountSid, twilioAccountToken);
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
+
+// App Init
 var app = express();
 
 // var twilio = require('twilio');
-// twilio.initialize("AC31fd04d3ce8f6369308dc42d2cb16559","f20e0042726b0d9cca9e017e68d1f579");
+// twilio.initialize(twilioAccountSid,twilioAccountToken);
 
 
 // Serve static assets from the /public folder
