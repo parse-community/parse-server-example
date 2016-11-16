3b0e3ce5b27dd0fa99cfd0ef23cd5f335797c718
Bin Lin <cold.lin@gmail.com>
2016-11-16 06:25:35 +0000

    add more counter

--- cloud/main.js
+++ cloud/main.js
@@ -46,6 +46,51 @@ Parse.Cloud.define("markFeedbackAsRead", function(request, response) {
 	});
 });
 
+Parse.Cloud.define("incrementFeaturedBookStats", function(request, response) {
+	var bookQuery =new Parse.Query("PublishedBook");
+
+	var bookId =request.params.bookRemoteId;
+	var bookLikeAddCount = request.params.likedTimes || 1;
+	var bookReadAddCount = request.params.readTimes || 1;
+	console.log("search with ids:"+bookId);
+	bookQuery.equalTo("objectId",bookId);
+	bookQuery.limit(1);
+	bookQuery.find({
+			useMasterKey:true,
+			success: function(results) {
+    		  	var book = results[0];
+    			book.increment("playedTimes", bookReadAddCount);
+    			book.increment("likedTimes", bookLikeAddCount);
+    			book.save(null, { useMasterKey: true });
+    			response.success("incrementBookStats with Book only");
+    		},
+    		error: function() {
+    			response.error("bookId doesn't exist!"+request.params.bookRemoteId);
+    		}
+	});
+});
+
+Parse.Cloud.define("incrementBookReport", function(request, response) {
+	var bookQuery =new Parse.Query("PublishedBook");
+
+	var bookId =request.params.bookRemoteId;
+	console.log("search with ids:"+bookId);
+	bookQuery.equalTo("objectId",bookId);
+	bookQuery.limit(1);
+	bookQuery.find({
+			useMasterKey:true,
+			success: function(results) {
+    		  	var book = results[0];
+    			book.increment("reportedTimes");
+    			book.save(null, { useMasterKey: true });
+    			response.success("incrementBookReport with Book only");
+    		},
+    		error: function() {
+    			response.error("bookId doesn't exist!"+request.params.bookRemoteId);
+    		}
+	});
+});
+
 Parse.Cloud.define("incrementFeaturedBookPlay", function(request, response) {
 	var bookQuery =new Parse.Query("PublishedBook");
 
