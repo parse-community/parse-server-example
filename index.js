47af22ea13e93dad3a2b2d09abaaa68d030fe05b
gdimoff <gdimoff@Pro.local>
2016-11-17 16:15:15 +0200

    db server url

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = 'mongodb://quotlr:quotemein@ds155097.mlab.com:55097/parse';
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'quotlr',
   masterKey: process.env.MASTER_KEY || 'master', //Add your master key here. Keep it secret!
