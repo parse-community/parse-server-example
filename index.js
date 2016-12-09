d069dabc2f82978d3e7319bc465341e9e1a8c6d6
Alexander Kaplan <alexkaplan.mail@gmail.com>
2016-12-09 13:17:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_qxr3fsf3:ddle35csperr52i6mnis3ff62e@ds119568.mlab.com:19568/heroku_qxr3fsf3',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || 'smartcityaccessibility',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  restKey: process.env.REST_KEY || '2139d-231cb2-738fe'
+  serverURL: process.env.SERVER_URL || 'https://smartcityaccessibility.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
