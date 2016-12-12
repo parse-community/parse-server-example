493a44aeffdef65340738fd23a119fedd04b9635
samllchiang <samllchiang@hotmail.com>
2016-12-12 12:28:28 +0800

    ---加入push函式到main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,19 +1,70 @@
+Parse.Cloud.define('hello', function(request, response) {
+   console.log("hellow LOG0"); 
+   var initValue = 0;
+   response.success('Hi NEW 0'+initValue);
+});
 
-// Android push test
+
+ Parse.Cloud.define("sendPushToChannels", function(request, response) {
+  
+  var pushChannel2Cloud = request.params.pushChannel2Cloud;
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo("deviceType", "android");
+  pushQuery.equalTo("channels", pushChannel2Cloud);
+  
+  Parse.Push.send({
+    channels: [ request.params.pushChannel2Cloud],
+    data:{
+		    location : request.params.location,
+			describe : request.params.describe,
+			ParseUserObjId : request.params.ParseUserObjId,
+			detectTime : request.params.detectTime,
+			detectPPM : request.params.detectPPM                                            
+		 }
+  }, { useMasterKey: true }).then(function() {
+      response.success("########Push was sent successfully.")
+  }, function(error) {
+      response.error("##########Push failed to send with error: " + error.message);
+  });
+});
+
+Parse.Cloud.define("sendPushToSingle", function(request, response) {
+  var senderUser = request.user;
+  var pushSingle2Cloud = request.params.pushSingle2Cloud;
+
+ 
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('username', request.params.pushSingle2Cloud);
+ 
+  // Send the push notification to results of the query
+  Parse.Push.send({
+    where: pushQuery,
+     data:{
+		    location : request.params.location,
+			describe : request.params.describe,
+			ParseUserObjId : request.params.ParseUserObjId,
+			detectTime : request.params.detectTime,
+			detectPPM : request.params.detectPPM                                            
+		 }
+  }, { useMasterKey: true }).then(function() {
+	   console.log("#### PUSH OK for : " + request.params.pushSingle2Cloud )
+      response.success("Push was sent successfully.")
+  }, function(error) {
+	   console.log("#### PUSH ERROR" + error.message);
+      response.error("Push failed to send with error: " + error.message);
+  });
+});
+
+
 Parse.Cloud.define('pushChannelTest', function(request, response) {
-
-  // request has 2 parameters: params passed by the client and the authorized user
+ 
   var params = request.params;
   var user = request.user;
-
-  // To be used with:
-  // https://github.com/codepath/ParsePushNotificationExample
-  // See https://github.com/codepath/ParsePushNotificationExample/blob/master/app/src/main/java/com/test/MyCustomReceiver.java
+ 
   var customData = params.customData;
   var launch = params.launch;
   var broadcast = params.broadcast;
 
-  // use to custom tweak whatever payload you wish to send
   var pushQuery = new Parse.Query(Parse.Installation);
   pushQuery.equalTo("deviceType", "android");
 
@@ -29,13 +80,11 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
       payload.broadcast = broadcast;
   }
 
-  // Note that useMasterKey is necessary for Push notifications to succeed.
-
   Parse.Push.send({
   where: pushQuery,      // for sending to a specific channel
   data: payload,
   }, { success: function() {
-     console.log("#### PUSH OK");
+     console.log("#### PUSH OK" + payload );
   }, error: function(error) {
      console.log("#### PUSH ERROR" + error.message);
   }, useMasterKey: true});
@@ -43,29 +92,32 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
   response.success('success');
 });
 
-// iOS push testing
-Parse.Cloud.define("iosPushTest", function(request, response) {
-
-  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
-  var params = request.params;
-  var user = request.user;
-
-  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
-  var messageText = params.text;
-
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
-
-  Parse.Push.send({
-    where: pushQuery, // Set our Installation query                                                                                                                                                              
-    data: {
-      alert: "Message: " + messageText
+Parse.Cloud.define("averageStars", function(request, response) {
+  var query = new Parse.Query("Review");
+  query.equalTo("movie", request.params.movie);
+  query.find({
+    success: function(results) {
+      var sum = 0;
+      for (var i = 0; i < results.length; ++i) {
+        sum += results[i].get("stars");
+      }
+      response.success(sum / results.length);
+    },
+    error: function() {
+      response.error("movie lookup failed");
     }
-  }, { success: function() {
-      console.log("#### PUSH OK");
-  }, error: function(error) {
-      console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-  response.success('success');
+  });
 });
+
+Parse.Cloud.run('averageStars', { movie: 'The Matrix' },{success: function(result) {console.log("sucess");}, error: function(error) { }}).then(function(ratings) {
+  console.log("ratings = "+ratings); 
+});
+
+//Parse.Cloud.beforeSave(Parse.User, function(request, response) {
+//  var user = request.object;
+//  if (!user.get("email")) {
+//    response.error("Every user must have an email address..........beforeSave");
+//  } else {
+//    response.success();
+//  }
+//});
