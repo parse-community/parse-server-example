f0e50e4c28a419cc918efb50d6a8e227b24a1ba0
Sefi Albo <sefialbo@gmail.com>
2016-12-12 19:32:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'parkingmangment',
+  appId: process.env.APP_ID || 'parkingmanagment',
   masterKey: process.env.MASTER_KEY || 'sefiandshay', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://mighty-waters-37073.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
