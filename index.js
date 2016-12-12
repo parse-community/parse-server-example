487b0b6b94bf8afabc93485f207e48c1d9107836
Sefi Albo <sefialbo@gmail.com>
2016-12-12 15:34:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'parkingmanagment',
-  masterKey: process.env.MASTER_KEY || 'sefishay', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'oren',
+  masterKey: process.env.MASTER_KEY || 'oren', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://mighty-waters-37073.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
