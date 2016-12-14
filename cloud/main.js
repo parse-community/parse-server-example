8dd2911f5eeda3dd9af81f1e1fe0c92225dd8684
Alex Kaplan <alexkaplan.mail@gmail.com>
2016-12-14 21:08:08 +0200

    added job to clean parking spaces

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,35 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.job('deleteOldParkings', function(request, status) {
+
+    // All access
+    Parse.Cloud.useMasterKey();
+
+    var today = new Date();
+	var minutes = 5;
+    var time = (minutes * 60 * 1000);
+    var expirationDate = new Date(today.getTime() - (time));
+
+    var query = new Parse.Query('ParkingSpaces');
+        query.lessThan('createdAt', expirationDate);
+
+        query.find().then(function (spots) {
+			var arrayLength = spots.length;
+			for (var i = 0; i < arrayLength; i++) {
+				if(spots[i].get('taken') == false){
+					spots[i].set('taken', null, {
+						success: function() {
+							
+						},
+						error: function(error) {
+							
+						}
+					});
+				}
+			}
+			status.success("done");
+        }, function (error) {});
+		
+});
