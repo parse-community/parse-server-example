9462b367830456977efa297531fbeb267aec501a
Kyle Parent <kjpparent@gmail.com>
2016-12-18 23:28:09 -0800

    Configure server for notifications

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
+  push: {
+    ios: {
+      pfx: 'pushcert.p12',
+      bundleId: 'com.RecCheckApp.Rec-Check-App',
+      production: false
+    }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
