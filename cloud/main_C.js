151b462936e9f624cfe4e8887061e8771541d537
samllchiang <samllchiang@hotmail.com>
2017-01-03 14:25:19 +0800

    ---修改index.js package.json加入.env

---
+++ cloud/main_C.js
@@ -0,0 +1,123 @@
+Parse.Cloud.define('hello', function(request, response) {
+   console.log("hellow LOG0"); 
+   var initValue = 0;
+   response.success('Hi NEW 0'+initValue);
+});
+
+
+ Parse.Cloud.define("sendPushToChannels", function(request, response) {
+  
+  var pushChannel2Cloud = request.params.pushChannel2Cloud;
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo("deviceType", "android");
+  pushQuery.equalTo("channels", pushChannel2Cloud);
+  
+  Parse.Push.send({
+    channels: [ request.params.pushChannel2Cloud],
+    data:{
+		    location : request.params.location,
+			describe : request.params.describe,
+			ParseUserObjId : request.params.ParseUserObjId,
+			detectTime : request.params.detectTime,
+			detectPPM : request.params.detectPPM                                            
+		 }
+  }, { useMasterKey: true }).then(function() {
+      response.success("########Push was sent successfully...20161219Channel")
+  }, function(error) {
+      response.error("##########Push failed to send with error: " + error.message);
+  });
+});
+
+Parse.Cloud.define("sendPushToSingle", function(request, response) {
+  var senderUser = request.user;
+  var pushSingle2Cloud = request.params.pushSingle2Cloud;
+
+ 
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('username', request.params.pushSingle2Cloud);
+ 
+  // Send the push notification to results of the query
+  Parse.Push.send({
+    where: pushQuery,
+     data:{
+		    location : request.params.location,
+			describe : request.params.describe,
+			ParseUserObjId : request.params.ParseUserObjId,
+			detectTime : request.params.detectTime,
+			detectPPM : request.params.detectPPM                                            
+		 }
+  }, { useMasterKey: true }).then(function() {
+	   console.log("#### PUSH OK for : " + request.params.pushSingle2Cloud )
+      response.success("Push was sent successfully...20161219Single")
+  }, function(error) {
+	   console.log("#### PUSH ERROR" + error.message);
+      response.error("Push failed to send with error: " + error.message);
+  });
+});
+
+
+Parse.Cloud.define('pushChannelTest', function(request, response) {
+ 
+  var params = request.params;
+  var user = request.user;
+ 
+  var customData = params.customData;
+  var launch = params.launch;
+  var broadcast = params.broadcast;
+
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo("deviceType", "android");
+
+  var payload = {};
+
+  if (customData) {
+      payload.customdata = customData;
+  }
+  else if (launch) {
+      payload.launch = launch;
+  }
+  else if (broadcast) {
+      payload.broadcast = broadcast;
+  }
+
+  Parse.Push.send({
+  where: pushQuery,      // for sending to a specific channel
+  data: payload,
+  }, { success: function() {
+     console.log("#### PUSH OK" + payload );
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
+
+Parse.Cloud.define("averageStars", function(request, response) {
+  var query = new Parse.Query("Review");
+  query.equalTo("movie", request.params.movie);
+  query.find({
+    success: function(results) {
+      var sum = 0;
+      for (var i = 0; i < results.length; ++i) {
+        sum += results[i].get("stars");
+      }
+      response.success(sum / results.length);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
+});
+
+Parse.Cloud.run('averageStars', { movie: 'The Matrix' },{success: function(result) {console.log("sucess");}, error: function(error) { }}).then(function(ratings) {
+  console.log("ratings = "+ratings); 
+});
+
+//Parse.Cloud.beforeSave(Parse.User, function(request, response) {
+//  var user = request.object;
+//  if (!user.get("email")) {
+//    response.error("Every user must have an email address..........beforeSave");
+//  } else {
+//    response.success();
+//  }
+//});
--- index _0.js
+++ index _0.js
@@ -3,12 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
-
-require('dotenv').config();// include .env data
-
-var logger = require('parse-server/lib/Adapters/Logger/FileLoggerAdapter').FileLoggerAdapter;
-
+ 
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI
 
@@ -52,12 +47,7 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   push: pushConfig,
   filesAdapter: filesAdapter,
-  serverURL: process.env.SERVER_URL || 'http://localhost/parse' , // needed for Parse Cloud and push notifications
-   loggerAdapter: new logger({ logsFolder: './' }),
-  verbose: true,
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  serverURL: process.env.SERVER_URL || 'http://localhost/parse'  // needed for Parse Cloud and push notifications
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -71,20 +61,10 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a web site...2017010301');
-});
-
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+  res.status(200).send('I dream of being a web site.');
 });
 //-----------------------------------xxx3
 var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
+app.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
-
-// This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
