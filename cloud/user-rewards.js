2515f701d727d1809676c279c3e1e8f233b0fc04
Bin Lin <cold.lin@gmail.com>
2017-01-06 23:10:12 +0000

    start user profile - using promise

--- cloud/user-rewards.js
+++ cloud/user-rewards.js
@@ -12,7 +12,7 @@ Parse.Cloud.define("UpdateUserStats", function(request, response) {
 	var userProfileQuery =new Parse.Query("UserProfile");
 	userProfileQuery.equalTo("username", username);
     userProfileQuery.limit(1);
-	promises.push(userProfileQuery.find());
+	promises.push(userProfileQuery.find({useMasterKey:true}));
 
 	Parse.Promise.when(promises).then(function(users,userProfiles){
        console.log("user:"+user.toJSON());
