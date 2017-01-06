31eca169941aee0af7448815fc957024d69caf4a
Bin Lin <cold.lin@gmail.com>
2017-01-06 23:03:57 +0000

    start user profile - using promise

--- cloud/user-rewards.js
+++ cloud/user-rewards.js
@@ -1,39 +1,36 @@
-// User login update
+// User profile update
 Parse.Cloud.define("UpdateUserStats", function(request, response) {
-
 	var username =request.params.username;
+
+	var promises = [];
+
 	var userQuery =new Parse.Query("_User");
-	console.log("search with username:"+username);
 	userQuery.equalTo("username",username);
-	userQuery.limit(1);
-	userQuery.find({useMasterKey:true})
-		.then(
-			function(results) {
-				var user = results[0];
-				if(user){
-					console.log("found user:"+user.get("email"));
-					var userProfileQuery =new Parse.Query("UserProfile");
-					userProfileQuery.equalTo("username",username);
-                    userProfileQuery.limit(1);
-					userProfileQuery.find({useMasterKey:true})
-							.then(function(results) {
-								var userProfile = results[0];
-								if(userProfile){
-									console.log("userProfile:"+userProfile.toJSON());
-									userProfile.set("test","test123")
-									response.success(userProfile.toJSON());
-								}else{
-									response.error("userProfile doesn't exist:"+username);
-								}
-							},
-							function(error) {
-							  response.error("failed to query UserProfile:"+error);
-							});
-				}else{
-					response.error("user doesn't exist:"+username);
-				}
-			},
-			function(error) {
-			  response.error("failed to query user by username:"+username);
-			});
+    userQuery.limit(1);
+	promises.push(userQuery.find({useMasterKey:true}));
+
+	var userProfileQuery =new Parse.Query("UserProfile");
+	userProfileQuery.equalTo("username", username);
+    userProfileQuery.limit(1);
+	promises.push(userQuery.find({userProfileQuery:true}));
+
+	Parse.Promise.when(promises).then(function(users,userProfiles){
+       console.log("user:"+user.toJSON());
+       console.log("userProfile:"+userProfile.toJSON());
+       var userProfile = results[0];
+       if(userProfile){
+			console.log("userProfile:"+userProfile.toJSON());
+			userProfile.set("test","test123")
+			response.success(userProfile.toJSON());
+		}else{
+			response.error("userProfile doesn't exist:"+username);
+		}
+    }, function(error){
+      	response.error("failed to query UserProfile:"+error);
+    })
+
+
+	console.log("search with username:"+username);
 });
+
+
