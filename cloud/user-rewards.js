a53ec8b02a94c016a95afa5b3fb93a62bad51de0
Bin Lin <cold.lin@gmail.com>
2017-01-06 19:07:16 +0000

    start user profile

--- cloud/user-rewards.js
+++ cloud/user-rewards.js
@@ -12,26 +12,7 @@ Parse.Cloud.define("UpdateUserStats", function(request, response) {
 				var user = results[0];
 				if(user){
 					console.log("found user:"+user.get("email"));
-					var userProfileQuery =new Parse.Query("UserProfile");
-					userProfileQuery.equalTo("username",username);
-                    userProfileQuery.limit(1);
-					userProfileQuery.find({useMasterKey:true})
-							.then(function(results) {
-								var userProfile = results[0];
-								if(userProfile){
-								console.log("userProfile:"+userProfile.toJSON());
-//									var result =  JSON.parse(userProfile.toJSON());
-//									result.test = "test123";
-//									console.log("result:"+result.toJSON());
-									userProfile.set("test","test123")
-									response.success(userProfile.toJSON());
-								}else{
-									response.error("userProfile doesn't exist:"+username);
-								}
-							},
-							function(error) {
-							  response.error("failed to query UserProfile:"+error);
-							});
+					updateUserProfile(user, response);
 				}else{
 					response.error("user doesn't exist:"+username);
 				}
@@ -57,3 +38,26 @@ Parse.Cloud.define("UpdateUserStats", function(request, response) {
 //    		}
 //	});
 });
+
+function updateUserProfile(user, response) {
+	var userProfileQuery =new Parse.Query("UserProfile");
+	userProfileQuery.equalTo("username",username);
+	userProfileQuery.limit(1);
+	userProfileQuery.find({useMasterKey:true})
+			.then(function(results) {
+				var userProfile = results[0];
+				if(userProfile){
+				console.log("userProfile:"+userProfile.toJSON());
+//									var result =  JSON.parse(userProfile.toJSON());
+//									result.test = "test123";
+//									console.log("result:"+result.toJSON());
+					userProfile.set("test","test123")
+					response.success(userProfile.toJSON());
+				}else{
+					response.error("userProfile doesn't exist:"+username);
+				}
+			},
+			function(error) {
+			  response.error("failed to query UserProfile:"+error);
+			});
+}
