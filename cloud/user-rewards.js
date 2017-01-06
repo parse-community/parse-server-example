a555b30d8170b173c27f84a21f54a23712bc31d5
Bin Lin <cold.lin@gmail.com>
2017-01-06 16:54:40 +0000

    start user reward

--- cloud/user-rewards.js
+++ cloud/user-rewards.js
@@ -6,28 +6,30 @@ Parse.Cloud.define("UpdateUserStats", function(request, response) {
 	console.log("search with username:"+username);
 	userQuery.equalTo("username",username);
 	userQuery.limit(1);
-//	userQuery.find().then(function(results) {
-//		if(results){}
-//		var user = results[0];
-//        response.success("user found: "+ user.get("email"));
-//        }else{
-//        }
-//       });
+	userQuery.find({useMasterKey:true}).then(function(results) {
+        var user = results[0];
+		if(user){
+			console.log("found user:"+user.get("email"));
+			response.success("user found: "+ user.get("email"));
+		}else{
+			response.error("user doesn't exist:"+username);
+		}
+       });
 
-	userQuery.find({
-			useMasterKey:true,
-			success: function(results) {
-    		  	var user = results[0];
-    		  	if(user){
-    		  		console.log("found user:"+user.get("email"));
-                	response.success("user found: "+ user.get("email"));
-    		  	}else{
-    		  		response.error("user doesn't exist:"+username);
-    		  	}
-    		},
-    		error: function() {
-    			console.log("not found user:"+username);
-    			response.error("user doesn't exist:"+username);
-    		}
-	});
+//	userQuery.find({
+//			useMasterKey:true,
+//			success: function(results) {
+//    		  	var user = results[0];
+//    		  	if(user){
+//    		  		console.log("found user:"+user.get("email"));
+//                	response.success("user found: "+ user.get("email"));
+//    		  	}else{
+//    		  		response.error("user doesn't exist:"+username);
+//    		  	}
+//    		},
+//    		error: function() {
+//    			console.log("not found user:"+username);
+//    			response.error("user doesn't exist:"+username);
+//    		}
+//	});
 });
