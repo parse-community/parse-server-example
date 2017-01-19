549077dc42f71de68df6e54d59dd6cc3195f3932
Kyle Parent <kjpparent@gmail.com>
2017-01-19 01:34:27 -0800

    Remove useMasterKey

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,6 @@
 Parse.Cloud.define("addCanonicalHandles", function(request, response) {
   var query = new Parse.Query("Profile");
   query.find({
-    useMasterKey: true,
     success: function(results) {
       var sum = 0;
       for (var i = 0; i < results.length; ++i) {
