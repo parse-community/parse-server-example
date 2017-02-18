539e1448d67fcd2e8a7cbc80e7027a1451232579
gasparandlola <gasparandlola@gmail.com>
2017-02-18 13:13:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'gl26845',
   masterKey: process.env.MASTER_KEY || 'gorillaTactics26845', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://gasparandlola.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
