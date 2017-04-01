d8028212d67828bffb4323d62168058705010a8d
AlonVIzel <alon.vizel@gmail.com>
2017-04-01 20:55:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'bracelet',
   masterKey: process.env.MASTER_KEY || 'alon', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://my-bracelet-app.herokuapp.com',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://my-bracelet-app.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
