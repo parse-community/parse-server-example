a154937fd43861f045310022b490ec9cfb25b572
AlonVIzel <alon.vizel@gmail.com>
2017-04-06 19:20:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
--app.get('/', function(req, res) {
- -  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub! Alon');
+app.get('/', function(req, res) {
+  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub! Alon');
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
