538ff2da3a45c809b0fd4d0f489addf0c8c0fd5f
sunFromAH <347502843@qq.com>
2017-06-01 15:03:12 +0800

    Update main.js
    可以根据时间、页数、点赞次数、播放次数查询符合条件的book，同时也可以删除这些书。

--- cloud/main.js
+++ cloud/main.js
@@ -7,6 +7,64 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi from anitales');
 });
 
+Parse.Cloud.define('queryTest',function(req, res){
+  	var bookQuery = new Parse.Query("PublishedBook");	
+	
+	var pages = req.params.pages;
+	var createdAt = req.params.createdAt;
+	var playedTimes = req.params.playedTimes;
+	var likedTimes = req.params.likedTimes;
+	var isDelete = req.params.isDelete;
+
+	console.log('req',req);
+	if( typeof(pages)!="undefined"){
+		console.log('加入参数pages');
+		bookQuery.lessThan("pages",pages-0);
+	}
+
+	if( typeof(likedTimes)!="undefined"){
+		console.log('加入参数likedTimes');
+		bookQuery.lessThan("likedTimes",likedTimes-0);
+	}
+
+	if( typeof(playedTimes)!="undefined"){
+		console.log('加入参数playedTimes');
+		bookQuery.lessThan("playedTimes",playedTimes-0);
+	}
+	
+	if( typeof(createdAt)!="undefined"){
+		console.log('加入参数createdAt');
+		var date = new Date(createdAt);
+		date.setDate(date.getDate()+1);
+		bookQuery.lessThan("createdAt",date);
+	}
+
+	if(isDelete!='true' ){
+
+		bookQuery.count({
+			success:function(number){
+		
+     				console.log('符合条件的文档数为：',number);
+				res.success('符合条件的文档数为：'+number);
+			},
+		
+			error:function(error){
+				res.error('请输入正确的参数');
+           		}
+		});
+	}else{
+		bookQuery.find({
+			success:function(args){
+				for(var i = 0; i < args.length ; i++){
+					book = args[i]
+					console.log(book);
+					book.destroy(book);//删除一本书
+				}
+				res.success('符合条件的文档数为：'+args.length+", 已经将这些数据删除");	
+			}
+		});	
+	}
+})
 
 Parse.Cloud.define("addBookVideoLink", function(request, response) {
 	var bookQuery =new Parse.Query("PublishedBook");
