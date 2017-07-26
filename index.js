f52a853807bcc35f9736768d131374239f9558b6
calebchiesa <email@orbyt.me>
2017-07-26 13:04:42 -0700

    Removes hardcoded values.

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || '12341234',
-  masterKey: process.env.MASTER_KEY || '43214321', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || '',
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
