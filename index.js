36b0f60bbcfe98239d97f8a77da3e99975285dac
dwijsoft-rahul <10132620+dwijsoft-rahul@users.noreply.github.com>
2018-04-04 22:07:03 -0400

    Env vars for push

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  push: {
+    android: process.env.ANDROID_PUSH,
+    ios: process.env.IOS_PUSH
+  },
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
