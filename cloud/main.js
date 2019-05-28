90c48e942ca8ae0e9b7b254712fe1841b0b03f4d
aalptekin <aalptekin@yahoo.com>
2019-05-28 23:30:54 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -30,14 +30,14 @@ Parse.Cloud.define("retrieveAllObjects", function(request, status) {
     };
     process(false);
 });
-Parse.Cloud.define(“sumSales", async (request) => {
-  const query = new Parse.Query(“sale”);
-  query.equalTo(“saleUser”, request.params.salesUser);
- query.equalTo(“status”, “S”);
+Parse.Cloud.define("sumSales", async (request) => {
+  const query = new Parse.Query("sale");
+  query.equalTo("saleUser", request.params.saleUser);
+ query.equalTo("status", "S");
   const results = await query.find();
   let sum = 0;
   for (let i = 0; i < results.length; ++i) {
-    sum += results[i].get(“saleamount”);
+    sum += results[i].get("saleamount");
   }
   return sum;
 });
