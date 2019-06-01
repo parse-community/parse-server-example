34f59aac7cd6c9e82bfc170bc9de8550bf0c69a9
aalptekin <aalptekin@yahoo.com>
2019-06-01 06:20:48 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -56,7 +56,7 @@ Parse.Cloud.define("sumBalances", function(request, response) {
   var query = new Parse.Query("balance");
     query.limit(10000);
   //Query balances with tokenid for the organization_event prefix
-  query.startsWith("tokenid", "TAC_HC2019_8100919");
+  query.startsWith("tokenid", request.params.tokenPrefix);
   query.find({
     success: function(results) {
       var sum = 0;
