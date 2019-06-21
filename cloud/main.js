52acb1266c52c8d18551f85edafa121ee5a2fef0
aalptekin <aalptekin@yahoo.com>
2019-06-21 11:06:42 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -32,7 +32,7 @@ Parse.Cloud.define("retrieveAllObjects", function(request, status) {
 });
 Parse.Cloud.define("sumSales", function(request, response) {
   //Query class appointments
-  message("Started function sumSales");
+  console.log("Started function sumSales");
   var query = new Parse.Query("sale");
     query.limit(10000);
   //Query column trainer in appointments pass trainerid object
@@ -42,10 +42,10 @@ Parse.Cloud.define("sumSales", function(request, response) {
       var sum = 0;
       for (var i = 0; i < results.length; ++i) {
         //Get the sum of the field rate for the trainer
-        message("in the loop");
+        console.log("in the loop");
         sum += results[i].get("saleamount");
       }
-      message("Success response message returned");
+      console.log("Success response message returned");
       response.success({value1 : sum, value2 : results.length});
       //response.success(results.length);  
     },
