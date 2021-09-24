1371757e8d912e5acf51376e62777fc51fcb4302
dblythy <daniel-blyth@live.com.au>
2021-09-24 14:45:25 +1000

    simplify testing

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ import { ParseServer } from 'parse-server';
 import { createServer } from 'http';
 import { config } from './src/config.js';
 import { renderFile } from 'ejs';
-const args = process.argv || [];
-const test = args.some(arg => arg.includes('jasmine'));
 
 export const app = express();
 app.set('view engine', 'ejs');
@@ -19,18 +17,17 @@ app.use('/public', express.static('./src/public'));
 
 // Serve the Parse API on the /parse URL prefix
 const mountPath = process.env.PARSE_MOUNT || '/parse';
-if (!test) {
-  const api = new ParseServer(config);
-  app.use(mountPath, api);
-}
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
   res.render('test.html', { appId: config.appId, serverUrl: config.serverURL });
 });
 
-const port = process.env.PORT || 1337;
-if (!test) {
+if (!process.env.TESTING) {
+  const api = new ParseServer(config);
+  app.use(mountPath, api);
+
+  const port = process.env.PORT || 1337;
   const httpServer = createServer(app);
   httpServer.listen(port, function () {
     console.log(`parse-server-example running on port ${port}.`);
