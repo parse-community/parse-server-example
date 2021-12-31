6b4af4c73c16d68d5d50ac44f55d2b17ce6a6646
astralkingdummy <cjansah11@gmail.com>
2021-12-30 22:46:58 -0500

    remove comment :desolate:

--- routes/scores.js
+++ routes/scores.js
@@ -1,14 +1,6 @@
 const Play = require("../models/play")
 const Profile = require("../models/profile")
 
-// function WeightPercentage(x)
-//     if x == 100 then return 110
-//     elseif x >= 90 then return -116640 + (64595/18)*x - (9937/270)*x^2 + (17/135)*x^3
-//     elseif x >= 85 then return 6040 - (851/6)*x + (5/6)*x^2
-//     elseif x >= 75 then return 0.5*x - 37.5
-//     else return 0 end
-// end
-
 const weightPercentage = (value) => {
     if (value === 100)
         return 110;
