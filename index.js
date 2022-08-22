49cf8f4ece808f7f55ee81855e52b0570f7781b8
pastorcoder <zuma4good@gmail.com>
2022-08-22 19:09:43 +0100

    still tryingout the S3Adapter

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const config = {
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
-  filesAdapter: s3Adapter,
+  // filesAdapter: s3Adapter,
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
