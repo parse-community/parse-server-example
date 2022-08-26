be17484fd5776d9acce0750cae7271e35224b5bf
pastorcoder <zuma4good@gmail.com>
2022-08-26 15:25:49 +0100

    fileUpload.enableForPublic =  true

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const config = {
 
 
 };
-fileUpload.enableForPublic;
+fileUpload.enableForPublic =  true;
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
