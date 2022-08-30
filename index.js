e4f80b92d066dd42520378eb8c4b0e938b94d74a
pastorcoder <zuma4good@gmail.com>
2022-08-30 12:46:14 +0100

    fileUpload.enableForPublic =  true in config

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const config = {
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  fileUpload: enableForPublic =  true,
   // filesAdapter: s3Adapter,
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
@@ -96,7 +97,7 @@ const config = {
 
 
 };
-fileUpload.enableForPublic =  true;
+// fileUpload.enableForPublic =  true;
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
