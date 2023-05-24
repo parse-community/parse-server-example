c08b163328d215b91091078ed342ff185e452787
Sayyad Gul Mohammad <syedgulmohammad25794@gmail.com>
2023-05-24 11:00:36 +0530

    user signup done

--- cloud/functions.js
+++ cloud/functions.js
@@ -1,6 +1,9 @@
+//
+const moment = require("moment-timezone");
+moment.tz.setDefault("Asia/Kolkata")
+
 Parse.Cloud.define('hello', req => {
-  console.log(req,"req");
-  // req.log.info(req);
+  req.log.info(req);
   return 'Hi';
 });
 
@@ -12,4 +15,34 @@ Parse.Cloud.define('asyncFunction', async req => {
 
 Parse.Cloud.beforeSave('Test', () => {
   throw new Parse.Error(9001, 'Saving test objects is not available.');
+});
+
+// End point for User Sign Up
+Parse.Cloud.define("signUp" , async ( req )=>{
+  try {
+    const { username , password , email } = req.params ;
+    const user = new Parse.User();
+
+    if( !( username && password && email ) ){
+      throw new Error("Missing Params");
+    }
+
+    const data = {
+      username,
+      password,
+      email:email.toLowerCase()
+    }
+
+    // user.set("username",username);
+    // user.set("password",password);
+    // user.set("email",email.toLowerCase());
+
+    // let result = await user.singUp(null,{useMasterKey:true}); in case already set the key value pair
+
+    let result = await user.signUp(data,{useMasterKey:true});
+    return result ;
+  } catch (error) {
+    console.error(error);
+    throw error ; 
+  }
 });
