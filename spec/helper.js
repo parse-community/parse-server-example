4d495f8091997eda46dd2c5b7485212ee0263db6
Manuel <5673677+mtrezza@users.noreply.github.com>
2023-06-06 00:53:58 +0200

    ci: Fix auto-release (#440)

--- spec/helper.js
+++ spec/helper.js
@@ -1,7 +1,15 @@
+import dns from 'dns';
 import { startParseServer, stopParseServer, dropDB } from './utils/test-runner.js';
+
+// Ensure localhost resolves to ipv4 address first on node v17+ for connection to MongoDB;
+// see https://www.mongodb.com/community/forums/t/econnrefused-27017/131911
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder('ipv4first');
+}
+
 beforeAll(async () => {
   await startParseServer();
-}, 100 * 60 * 2);
+}, 100 * 60 * 20);
 
 afterAll(async () => {
   await dropDB();
