94a65769a01edcb1a37898208db8f558a060d23d
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2023-06-06 00:27:12 +0200

    Update helper.js

--- spec/helper.js
+++ spec/helper.js
@@ -1,7 +1,7 @@
 import { startParseServer, stopParseServer, dropDB } from './utils/test-runner.js';
 beforeAll(async () => {
   await startParseServer();
-}, 100 * 60 * 2);
+}, 100 * 60 * 20);
 
 afterAll(async () => {
   await dropDB();
