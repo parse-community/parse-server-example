bfea23671145019e53f3dd0281afced09fc983eb
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2023-06-06 00:34:18 +0200

    Update helper.js

--- spec/helper.js
+++ spec/helper.js
@@ -1,4 +1,12 @@
+import dns from 'dns';
 import { startParseServer, stopParseServer, dropDB } from './utils/test-runner.js';
+
+// Ensure localhost resolves to ipv4 address first on node v17+ for connection to MongoDB;
+// see https://www.mongodb.com/community/forums/t/econnrefused-27017/131911
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder('ipv4first');
+}
+
 beforeAll(async () => {
   await startParseServer();
 }, 100 * 60 * 20);
