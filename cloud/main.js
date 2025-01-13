dc96e9d79466d25bb7e9d989f3dfe780912d4305
Daniel <daniel-blyth@live.com.au>
2025-01-13 20:32:49 +1100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,2 +1,2 @@
 // It is best practise to organize your cloud functions group into their own file. You can then import them in your main.js.
-import('./functions.js');
+await import('./functions.js');
