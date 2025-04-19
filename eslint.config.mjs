7f5b807b86e0017a20909fbbf198433325fe8751
Abubkr Hago <24502430+abubkr-hago@users.noreply.github.com>
2025-04-20 02:37:00 +0400

    fix: apply eslint to the whole project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,37 +5,43 @@ import path from 'node:path';
 
 const __dirname = path.resolve();
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
-  files: ['**/*.ts'],
-  ignores: ['./public/**/*', 'release.config.js'],
-  languageOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-    parser: tseslint.parser,
-    parserOptions: {
-      project: ['./tsconfig.json', './spec/tsconfig.json'],
-      tsconfigRootDir: __dirname,
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json', './spec/tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+      globals: {
+        ...globals.node,
+        Parse: 'readonly',
+      },
     },
-    globals: {
-      ...globals.node,
-      Parse: 'readonly',
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      'no-trailing-spaces': 'error',
+      'eol-last': 'error',
+      'space-in-parens': ['error', 'never'],
+      'no-multiple-empty-lines': 'warn',
+      'prefer-const': 'error',
+      'space-infix-ops': 'error',
+      'no-useless-escape': 'off',
+      'require-atomic-updates': 'off',
+      'no-var': 'warn',
+      'no-await-in-loop': 'warn',
     },
   },
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'linebreak-style': ['error', 'unix'],
-    'no-trailing-spaces': 'error',
-    'eol-last': 'error',
-    'space-in-parens': ['error', 'never'],
-    'no-multiple-empty-lines': 'warn',
-    'prefer-const': 'error',
-    'space-infix-ops': 'error',
-    'no-useless-escape': 'off',
-    'require-atomic-updates': 'off',
-    'no-var': 'warn',
-    'no-await-in-loop': 'warn',
-  },
-});
+  {
+    ignores: ['dist/**/*', 'logs/**/*', 'public/**/*', 'release.config.js'],
+  }
+);
