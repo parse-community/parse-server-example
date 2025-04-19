7b03f63ca24d6aafb45ee81a712c51dc745ed788
Abubkr Hago <24502430+abubkr-hago@users.noreply.github.com>
2025-04-20 01:21:21 +0400

    refactor: move express routes outside server start promise

--- index.ts
+++ index.ts
@@ -12,32 +12,29 @@ const app = express();
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function (req, res) {
+  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+});
+
+// There will be a test page available on the /test path of your server url
+// Remove this before launching your app
+app.get('/test', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/test.html'));
+});
+
 // Serve the Parse API on the /parse URL prefix
 const mountPath = process.env.PARSE_MOUNT || '/parse';
 const server = new ParseServer(config);
 
-server.start().then(() => {
+server.start().then(async () => {
   app.use(mountPath, server.app);
-
-  // Parse Server plays nicely with the rest of your web routes
-  app.get('/', function (req, res) {
-    res
-      .status(200)
-      .send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-  });
-
-  // There will be a test page available on the /test path of your server url
-  // Remove this before launching your app
-  app.get('/test', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/test.html'));
-  });
-
   const port = process.env.PORT || 1337;
   const httpServer = http.createServer(app);
   httpServer.listen(port, function () {
     console.log('parse-server-example running on port ' + port + '.');
   });
-  // This will enable the Live Query real-time server
   console.log(`Visit http://localhost:${port}/test to check the Parse Server`);
-  return ParseServer.createLiveQueryServer(httpServer);
+  // This will enable the Live Query real-time server
+  await ParseServer.createLiveQueryServer(httpServer);
 });
