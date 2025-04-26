cfa5f836701ad0a63470d757e1587844ed776ddd
Abubkr Hago <24502430+abubkr-hago@users.noreply.github.com>
2025-04-26 17:02:14 +0400

    feat: increase module and target to support more features

--- index.ts
+++ index.ts
@@ -7,10 +7,17 @@ import path from 'path';
 import http from 'http';
 import { config } from './config.js';
 
+const __dirname = path.resolve();
 const app = express();
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Serve the Parse API on the /parse URL prefix
+const mountPath = process.env.PARSE_MOUNT || '/parse';
+const server = new ParseServer(config);
+await server.start();
+app.use(mountPath, server.app);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
@@ -23,18 +30,11 @@ app.get('/test', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
-// Serve the Parse API on the /parse URL prefix
-const mountPath = process.env.PARSE_MOUNT || '/parse';
-const server = new ParseServer(config);
-
-server.start().then(async () => {
-  app.use(mountPath, server.app);
-  const port = process.env.PORT || 1337;
-  const httpServer = http.createServer(app);
-  httpServer.listen(port, function () {
-    console.log('parse-server-example running on port ' + port + '.');
-  });
-  console.log(`Visit http://localhost:${port}/test to check the Parse Server`);
-  // This will enable the Live Query real-time server
-  await ParseServer.createLiveQueryServer(httpServer);
+const port = process.env.PORT || 1337;
+const httpServer = http.createServer(app);
+httpServer.listen(port, function () {
+  console.log('parse-server-example running on port ' + port + '.');
 });
+// This will enable the Live Query real-time server
+await ParseServer.createLiveQueryServer(httpServer);
+console.log(`Visit http://localhost:${port}/test to check the Parse Server`);
