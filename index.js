3a7ac972c7269f8f640152775cd534ef105f5259
odensutibun <odensutibun@gmail.com>
2025-11-19 15:20:38 +0100

    Update import statement for config file extension

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import express from 'express';
 import { ParseServer } from 'parse-server';
 import path from 'path';
 import http from 'http';
-import { config } from './config';
+import { config } from './config.js';
 
 const __dirname = path.resolve();
 const app = express();
